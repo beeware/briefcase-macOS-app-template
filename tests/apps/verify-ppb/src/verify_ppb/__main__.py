2aa7dff5f105d63d46c48a4e7c53377f816000e5
Drew Winstel <drew@thenounproject.com>
2022-10-20 14:25:12 -0700

    Add CI
    1. Copy Xcode CI and test app
    2. Switch Xcode build step to be the plain app step
    3. Update pyproject.toml to remove xcode from the config

---
+++ tests/apps/verify-ppb/src/verify_ppb/__main__.py
@@ -0,0 +1,4 @@
+from verify_ppb.app import main
+
+if __name__ == '__main__':
+    main()
---
+++ tests/apps/verify-pyside2/src/verify_pyside2/app.py
@@ -0,0 +1,44 @@
+"""
+A PySide2 test app
+"""
+import sys
+
+try:
+    from importlib import metadata as importlib_metadata
+except ImportError:
+    # Backwards compatibility - importlib.metadata was added in Python 3.8
+    import importlib_metadata
+
+from PySide2 import QtWidgets
+
+
+class HelloPySide2(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('verify-pyside2')
+        self.show()
+
+
+def main():
+    # Linux desktop environments use app's .desktop file to integrate the app
+    # to their application menus. The .desktop file of this app will include
+    # StartupWMClass key, set to app's formal name, which helps associate
+    # app's windows to its menu item.
+    #
+    # For association to work any windows of the app must have WMCLASS
+    # property set to match the value set in app's desktop file. For PySide2
+    # this is set with setApplicationName().
+
+    # Find the name of the module that was used to start the app
+    app_module = sys.modules['__main__'].__package__
+    # Retrieve the app's metadata
+    metadata = importlib_metadata.metadata(app_module)
+
+    QtWidgets.QApplication.setApplicationName(metadata['Formal-Name'])
+
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = HelloPySide2()
+    sys.exit(app.exec_())
