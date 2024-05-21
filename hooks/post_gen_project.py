889248f4546003b150975774a4d496c70b0b2114
Claire Valliant <alcarithemad@gmail.com>
2024-05-21 16:56:50 -0400

    Rewrite Entitlements.plist to avoid newline issues.
    Git can be configured globally to mangle newlines to CRLF.
    Cookiecutter manages the git clone, so there's no easy way for us to
    set the configuration at the repository level during the templating
    process.
    
    The macOS utility `codesign` fails with obscure, hard to debug errors
    when presented with CRLF newlines in Entitlements.plist.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,3 +10,12 @@ os.rename(BIN_PATH / 'Stub-{{ cookiecutter.python_version|py_tag }}', BIN_PATH /
 # Delete all remaining stubs
 for stub in BIN_PATH.glob("Stub-*"):
     os.unlink(stub)
+
+# The codesign utility in recent macOS fails with obscure errors when presented with
+# CRLF line endings, but in some configurations (e.g. global `core.autocrlf=true`)
+# git may have checked out this repo in a way that put CRLF line endings in Entitlements.plist.
+# The following is thus a no-op most of the time, but it's a simple rewrite of a small file.
+ENTITLEMENTS_PATH = Path("Entitlements.plist")
+# This implicitly uses "universal" newlines mode.
+xml_content = ENTITLEMENTS_PATH.read_text()
+ENTITLEMENTS_PATH.open('w', newline='\n').write(xml_content)
